d86905c104e2bf842dc6ae9a9fe05a02c31bc2f9
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-08-29 13:31:30 +0200

    [#48204] Added script installing the dependencies
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

---
+++ .github/scripts/install-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright (c) 2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+apt-get update
+apt-get -qq -y install nodejs npm git
+npm install -g n
+n latest
+
+PIPELINE_MANAGER_REPO="${PIPELINE_MANAGER_REPO:-https://github.com/antmicro/kenning-pipeline-manager.git}"
+
+if [ ! -d "./kenning-pipeline-manager" ]
+then
+    git clone $PIPELINE_MANAGER_REPO ./kenning-pipeline-manager
+    pip3 install -e ./kenning-pipeline-manager
+fi
+
+echo "kenning-pipeline-manager version: $(git -C ./kenning-pipeline-manager rev-parse HEAD)" | tee deps.hashes
